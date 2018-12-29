2c1ed823222b6f4b871a097ace3928667760f5d5
Brian Jubelirer <brian2386@gmail.com>
2018-12-29 01:02:49 -0500

    add dpi scaling

--- client/init.js
+++ client/init.js
@@ -1,7 +1,26 @@
 var socket = io();
 
-//canvas
+
+
+
+//get DPI
+let dpi = window.devicePixelRatio;
+//get canvas
+let canvas = document.getElementById('ctx');
+//get context
+let ctx = canvas.getContext('2d');
+function fix_dpi() {
+	//get CSS height
+	//the + prefix casts it to an integer
+	//the slice method gets rid of "px"
+	let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+	//get CSS width
+	let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+	//scale the canvas
+	canvas.setAttribute('height', style_height * dpi);
+	canvas.setAttribute('width', style_width * dpi);
+
+}
+
 var size = 64;
-var canvas = document.getElementById("ctx")
-var ctx = canvas.getContext("2d");
-ctx.font = '30px Arial';
+var reachDistance = 75
