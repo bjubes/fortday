5515cb6998916dda5f101c32e6d47bfba4dbeb64
Brian Jubelirer <brian2386@gmail.com>
2018-12-29 01:20:10 -0500

    fix canvas scaling

--- client/init.js
+++ client/init.js
@@ -13,14 +13,14 @@ function fix_dpi() {
 	//get CSS height
 	//the + prefix casts it to an integer
 	//the slice method gets rid of "px"
-	let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+	let style_height = 500
 	//get CSS width
-	let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+	let style_width = 500
 	//scale the canvas
 	canvas.setAttribute('height', style_height * dpi);
 	canvas.setAttribute('width', style_width * dpi);
 
 }
-
+fix_dpi()
 var size = 64;
 var reachDistance = 75
